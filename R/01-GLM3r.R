c48bac76ff708e445f6a9941b80755104f41cf04
hdugan <hilarydugan@gmail.com>
2019-11-07 11:08:51 -0500

    Updated disclaimer on loading package

---
+++ R/01-GLM3r.R
@@ -0,0 +1,17 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("
+DISCLAIMER AND COPYRIGHT NOTICE
+By downloading the software you agree to the following terms:
+
+The University of Western Australia has developed the code with reasonable care and 
+skill, however, the authors do not make or imply any warranty as to the quality of 
+simulated output from the software supplied. Nor do the authors accept any 
+responsibility for the use of the model including any advice or information 
+which the user may give, or opinions which they express, arising from model 
+simulated output.
+
+This information is preliminary or provisional and is subject to revision. It
+is being provided to meet the need for timely best science.")
+}
+
+
