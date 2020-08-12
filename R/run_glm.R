bef82cf9f5a13c67c6f26b470defcf220d18d45b
jsta <stachel2@msu.edu>
2020-08-12 10:33:57 -0400

    add warning about setting glm_path via env variable

--- R/run_glm.R
+++ R/run_glm.R
@@ -62,6 +62,9 @@ glm.systemcall <- function(sim_folder, glm_path, verbose, system.args) {
   
   if(nchar(Sys.getenv("GLM_PATH")) > 0){
     glm_path <- Sys.getenv("GLM_PATH")
+    warning(paste0(
+      "Custom path to GLM executable set via 'GLM_PATH' environment variable as: ", 
+      glm_path))
   }
   
   origin <- getwd()
