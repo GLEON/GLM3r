e0a0cfa316367c06f5dbe7a8da56c7e4fab7e0c3
Jordan S. Read <jread@usgs.gov>
2021-12-16 08:29:59 -0600

    fix for different versions of GLM w/ diff message locations

--- R/glm_version.R
+++ R/glm_version.R
@@ -15,11 +15,13 @@
 #'@export
 glm_version <- function(as_char = FALSE){
   if (as_char){
-    stdout_ver_line <- run_glm(dirname(nml_template_path()), verbose=NULL, system.args='--help')[3]
-    if (!grepl('Version', stdout_ver_line, fixed = TRUE)){
+    messg_out <- run_glm(dirname(nml_template_path()), verbose=NULL, system.args='--help')
+    # only check the first 6 lines to avoid mistakenly catching words that are in the doc flags
+    version_line <- grepl('Version', messg_out[1:6], fixed = TRUE)
+    if (!any(version_line)){
       stop('"Version" not found in the expected message from GLM, try `as_char = FALSE`')
     }
-    out <- regmatches(stdout_ver_line, regexec("(?<=Version\ ).+?(?=\ )", stdout_ver_line, perl = TRUE))[[1]]
+    out <- regmatches(messg_out[version_line][1L], regexec("(?<=Version\ ).+?(?=\ )", messg_out[version_line][1L], perl = TRUE))[[1]]
     return(out)
   } else {
     run_glm(dirname(nml_template_path()), verbose=TRUE, system.args='--help')
